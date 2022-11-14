9dfd88678e609e3dfcc985caf47ac25a7f581279
kaitwillows <lunaeclipse7777@gmail.com>
2022-11-14 18:29:10 +0000

    automated commit

--- ArrayList/Main.java
+++ ArrayList/Main.java
@@ -4,7 +4,7 @@ class Main {
   public static void main(String[] args) {
     ArrayList<String[]>  test = new ArrayList<String[]>();
     String[] template = 
-      {"[ISSUE DESCRIPTION]", "[IDENTIFIED BY]", "[DATE IDNETIFIED]", "[RESOLVED BY]", "[DATE]",};
+      {"[ISSUE SUMMARY]", "[IDENTIFIED BY]", "[DATE IDNETIFIED]", "[RESOLVED BY]", "[DATE]", "[LONG DESCRIPTION"};
     test.add(template);
     System.out.println(test);
   }
@@ -15,6 +15,15 @@ class Main {
 }
 
 /* 
+from menu: (all methods essensially)
+- print all issues
+  - or unresolved
+- print specific issue in plain english
+- add issue
+  - loop for each thingy
+- mark as resolved
+  - pull time and user info
+
 - use the first index as a save name?
 - active ussue tracker 
 - show all or only unresolved issues
--- commit.sh
+++ commit.sh
@@ -1,3 +1,4 @@
+# stage commit and push all changes
 git add .
 git status
 git commit -m "automated commit"
