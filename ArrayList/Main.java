f1a7e263cd9f9078216dab2783d73e6ecb3974ed
kaitwillows <lunaeclipse7777@gmail.com>
2022-11-15 07:39:45 -0700

    automated commit

--- ArrayList/Main.java
+++ ArrayList/Main.java
@@ -4,9 +4,10 @@ class Main {
   public static void main(String[] args) {
     ArrayList<String[]>  test = new ArrayList<String[]>();
     String[] template = 
-      {"[ISSUE SUMMARY]", "[IDENTIFIED BY]", "[DATE IDNETIFIED]", "[RESOLVED BY]", "[DATE]", "[LONG DESCRIPTION"};
+      {"[ISSUE]", "[IDENTIFIED BY]", "[DATE IDNETIFIED]", "[RESOLVED BY]", "[DATE]", "[LONG DESCRIPTION]"};
     test.add(template);
-    System.out.println(test);
+    // System.out.println(test);
+    printIssues(test, "all");
   }
 
   static void duplicateArray() {
@@ -17,18 +18,53 @@ class Main {
     
   }
 
+  // INFO: pass "all", or "unresolved" as issue to print all items from the respective sets
   static void printIssues(ArrayList<String[]> dataStruct, String issue) {
-    // if issue is all or unresolved, loop through printing all
-    // else loop through untill the issue requested is found
-    // strcomp 0 = the same
-    if (issue.compareTo("all") == 0) {
+    if (issue.compareTo("all") == 0) { // prints all issues, reguardless of status
       System.out.println("printing all issues...\n");
-      for (String[] i : dataStruct) {
-        for (String j : i) {
-          System.out.println(j);
+      for (String[] i : dataStruct) { // each issue
+        for (String j : i) { // each item in issue
+          System.out.print(j + ", ");
         }
+        System.out.print("\n");
+      }
+    } else if (issue.compareTo("unresolved") == 0) { // prints only unresolved issues
+      boolean allResolved = true;
+      System.out.println("printing unresolved issues...\n");
+      for (String[] i : dataStruct) {
+        if (i[3].compareTo("(unresolved)") == 0) {
+          for (String j : i) {
+            System.out.print(j + ", ");
+          }
+          allResolved = false;
+        }
+        System.out.print("\n");
+      }
+      if (allResolved == true) {
+        System.out.println("\nall issues have been resolved, yay!!!"); // all issues resolved
+      }
+    } else { // find the specific issue requested
+      int duplicateIssue = 0;
+      for (String[] i : dataStruct) {
+        if (i[0].compareTo(issue) == 0) {
+          for (String j : i) {
+            System.out.print(j + ", ");
+          }
+          duplicateIssue++;
+        }
+        System.out.print("\n");
+      }
+      switch (duplicateIssue) {
+        case 0:
+          System.out.println("issue not found.");
+          break;
+        case 1:
+          break;
+        default: // duplicate case(s)
+          System.out.println("duplicate cases found, please address this.");
       }
     }
+
   }
 }
 
