37e518cfa0ac59cbf6bb03597ef6790e7f324b49
kaitwillows <lunaeclipse7777@gmail.com>
2022-11-21 07:14:02 -0700

    automated commit

--- BP37/Q10.java
+++ BP37/Q10.java
@@ -2,6 +2,7 @@ public class Q10 {
   public static void main(String[] args) {
     try {
       String carName = car(0);
+      System.out.println(carName);
     }
     catch (ArithmeticException e) {
       System.out.println("invalid input.");
@@ -10,6 +11,7 @@ public class Q10 {
 
   static String car(int xs) {
     int number = 5 / xs;
+    System.out.println(number);
     return "folkswagon"; // just for fun
   }
 }
