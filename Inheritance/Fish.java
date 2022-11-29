3ccf5a59df0bb49dba8cb7f17146c12cd09e97e8
kaitwillows <lunaeclipse777@gmail.com>
2022-11-29 09:23:35 -0700

    automated commit

--- Inheritance/Fish.java
+++ Inheritance/Fish.java
@@ -1,23 +1,18 @@
 
-/**
- *
- */
+
 public class Fish {
 	//Instant fields.
 	private int age;
 	private String name;
 	
-	/**
-	 * Constructor that takes no parameter.
-	 */
+
+	// Constructor that takes no parameter.
 	public Fish(){
 		name = "Fish.";
 		age = 1;
 	}
 	
-	/**
-	 * Constructor.
-	 */
+	// Constructor.
 	public Fish(int age, String name){
 		this.age = age;
 		this.name = name;
@@ -25,31 +20,25 @@ public class Fish {
 	
 	
 	
-	/**
-	 *overwrite this method in sub-class
-	 */
+
+	// overwrite this method in sub-class
 	public void greeting(){
 		System.out.println("Hi, I am " + name + ". I can swim.");
 	}
 	
-	/**
-	 * overwrite default toString method.
-	 * Print out this fish's name and age.
-	 */
+	// overwrite default toString method.
+	// Print out this fish's name and age.
 	public String toString(){
 		return "Name: " + name + " Age: " + age;
 	}
 	
-	/**
-	 * Main method for demoing and testing purposes
-	 * @param args
-	 */
+	
+	// Main method for demoing and testing purposes
 	public static void main(String[] args){
-		//Uncomment the code when use.
-		/*
-		Fish fish = new ClassName(); //use the name of the class that you create to replace ClassName.
-		fish.greeting();
-		fish.someMethod();  //use the method that you write to replace someMethod.
-		*/
+		// Uncomment the code when use.
+		// Fish fish = new ClassName(); //use the name of the class that you create to replace ClassName.
+		// fish.greeting();
+		// fish.someMethod();  //use the method that you write to replace someMethod.
+
 	}
 } //The end of Fish.java
