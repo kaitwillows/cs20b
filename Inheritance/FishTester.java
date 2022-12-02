0a03d63d1c0d00b6890f35d7d16d0d755129152e
kaitwillows <lunaeclipse777@gmail.com>
2022-12-02 10:23:45 -0700

    automated commit

--- Inheritance/FishTester.java
+++ Inheritance/FishTester.java
@@ -17,12 +17,30 @@ public class FishTester {
 
 
     ArrayList<Fish> fishList = new ArrayList<Fish>();
+    fishList.add(joe);
+    fishList.add(mike);
     while (true) {
-     // menu 
-      System.out.print("menu:\n  [1] print a list of all the fish\n  [2] observe fish\n  [3] add a fish\n  [4] quit");
+      // menu 
+      System.out.print("\nmenu:\n  [1] print a list of all the fish\n  [2] observe fish\n  [3] add a fish\n  [4] quit\n");
       int menuSelection = intScanner.nextInt();
+      System.out.println("");
       switch (menuSelection) {
-        case 3:
+        case 1: // prints the fish in a list
+          for (Fish i : fishList) {
+            System.out.println(i.toString());
+          }
+          break;
+        case 2: // shows the fishes greeting and methods
+          System.out.println(joe.toString());
+          joe.greeting();
+          joe.fly();
+          System.out.println("");
+
+          System.out.println(mike.toString());
+          mike.greeting();
+          System.out.println("");
+          break;
+        case 3: // adds fish based on user inputs 
           // add fish (in switch statement later idk)
           System.out.print("adding a fish...\nhow old is the fish? ");
           int age = intScanner.nextInt();
@@ -31,7 +49,7 @@ public class FishTester {
 
           fishList.add(new Fish(age, name));
           break;
-        case 4:
+        case 4: // allows the user to quit the program
           System.exit(0);
       }
 
