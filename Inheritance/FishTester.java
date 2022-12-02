36b542212fc63d4051d26d73b13cefa65bc047cb
kaitwillows <lunaeclipse7777@gmail.com>
2022-12-02 06:25:03 -0700

    automated commit

--- Inheritance/FishTester.java
+++ Inheritance/FishTester.java
@@ -6,18 +6,35 @@ public class FishTester {
     Scanner intScanner = new Scanner(System.in);
     Scanner strScanner = new Scanner(System.in);
     FlyFish joe = new FlyFish(23, "joe");
-    joe.fly();
-    joe.greeting();
-    System.out.println(joe.toString());
+    // joe.fly();
+    // joe.greeting();
+    // System.out.println(joe.toString());
 
     Fish mike = new Fish(37, "mike");
-    mike.greeting();
-    System.out.println(mike.toString());
+    // mike.greeting();
+    // System.out.println(mike.toString());
 
 
 
     ArrayList<Fish> fishList = new ArrayList<Fish>();
     while (true) {
+     // menu 
+      System.out.print("menu:\n  [1] print a list of all the fish\n  [2] observe fish\n  [3] add a fish\n  [4] quit");
+      int menuSelection = intScanner.nextInt();
+      switch (menuSelection) {
+        case 3:
+          // add fish (in switch statement later idk)
+          System.out.print("adding a fish...\nhow old is the fish? ");
+          int age = intScanner.nextInt();
+          System.out.print("what is the fishes name? ");
+          String name = strScanner.nextLine();
+
+          fishList.add(new Fish(age, name));
+          break;
+        case 4:
+          System.exit(0);
+      }
+
       /*
       menu:
         add fish
@@ -25,13 +42,7 @@ public class FishTester {
         watch fish (make a random method happen idk);
       */
 
-      // add fish (in switch statement later idk)
-      System.out.print("adding a fish...\nhow old is the fish? ");
-      int age = intScanner.nextInt();
-      System.out.print("what is the fishes name? ");
-      String name = strScanner.nextLine();
 
-      fishList.add(new Fish(age, name));
     }
     
   }
