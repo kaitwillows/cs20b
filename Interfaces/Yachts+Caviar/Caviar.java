b76562af9461f6619c5f4095f1db9bc487e2dae4
kaitwillows <lunaeclipse777@gmail.com>
2022-12-06 10:05:07 -0700

    automated commit

--- Interfaces/Yachts+Caviar/Caviar.java
+++ Interfaces/Yachts+Caviar/Caviar.java
@@ -1,4 +1,9 @@
 class Caviar extends Food implements LuxuryItem {
+  public Caviar(String name, double cost, int calories) {
+    super(name, cost, calories);
+  }
   String origin;
-  
+  public double calculateLuxuryTax() {
+    return 7;
+  }
 }
