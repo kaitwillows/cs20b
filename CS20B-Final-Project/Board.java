b8a0a5c79bb2dee8ad733fcc56102654cff4b344
kaitwillows <lunaeclipse777@gmail.com>
2022-12-12 11:10:03 -0700

    automated commit

---
+++ CS20B-Final-Project/Board.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+class Board {
+  public static void main(String[] args) {
+    JFrame board = new JFrame("test");
+    board.setSize(512,512);
+    board.setVisible(true);
+  }
+}
