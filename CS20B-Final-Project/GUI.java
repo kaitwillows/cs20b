337ab4495c1dbc5d5f18ca39be60a751b3a709f8
kaitwillows <lunaeclipse777@gmail.com>
2022-12-19 11:09:33 -0700

    automated commit

--- CS20B-Final-Project/GUI.java
+++ CS20B-Final-Project/GUI.java
@@ -1,10 +1,73 @@
-import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
 
 class GUI {
+  public static int x;
+  public static int y;
+  public static boolean whiteSquare = true;
+
   public static void main(String[] args) {
     JFrame board = new JFrame("test");
     board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    board.setSize(512,512);
+    board.setSize(512, 512);
     board.setVisible(true);
+
+    ImagePanel painter = new ImagePanel();
+
+    boolean whiteSquare = true;
+    x = 0;
+    y = 0;
+    board.add(painter);
+
+
+    // set variables
+    for (int i = 0; i < 7; i++) {
+      for (int j = 0; j < 7; j++) {
+        x = i * 64;
+        y = j * 64;
+        // board.add(painter);
+        if (whiteSquare) { // flip flop
+
+          whiteSquare = false;
+        } else {
+
+          whiteSquare = true;
+        }
+      }
+    } 
+
+
+
+  }
+}
+
+class ImagePanel extends JPanel {
+  private Image whiteImage;
+  private Image blackImage;
+  public ImagePanel() {
+    try {
+      whiteImage = ImageIO.read(new File("./white-square.png"));
+      blackImage = ImageIO.read(new File("./black-square.png"));
+    } catch (IOException ex) {
+      System.out.println("error at GUI:~29");
+    }
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    if (GUI.whiteSquare) {
+      g.drawImage(whiteImage, GUI.x, GUI.y, this);
+    } else {
+      g.drawImage(blackImage, GUI.x, GUI.y, this);
+    }
   }
 }
--- CS20B-Final-Project/Main.java
+++ CS20B-Final-Project/Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
+    System.out.println("how does print work");
     GUI.main(null);
     // Board gameObj = new Board(true);
     // gameObj.printBoard();
