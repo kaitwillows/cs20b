afd8f168bfb5355412dc269be685fecf21f7650f
kaitwillows <lunaeclipse777@gmail.com>
2022-12-20 09:58:39 -0700

    automated commit

---
+++ CS20B-Final-Project/ChessException.java
@@ -0,0 +1,12 @@
+class KingAttackedException extends RuntimeException {
+  public KingAttackedException(String errorMessage) {
+    super(errorMessage);
+  }
+}
+
+class IllegalMoveException extends RuntimeException {
+  public IllegalMoveException(String errorMessage) {
+    super(errorMessage);
+  }
+}
+
--- CS20B-Final-Project/Main.java
+++ CS20B-Final-Project/Main.java
@@ -1,11 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("how does print work");
-    GUI.main(null);
-    // Board gameObj = new Board(true);
-    // gameObj.printBoard();
-    // gameObj.testMove();
-    // gameObj.printBoard();
+    // System.out.println("how does print work");
+    // GUI.main(null);
+    Board gameObj = new Board(true);
+    gameObj.printBoard(true);
+    System.out.println("");
+    gameObj.printBoard(false);
 
   }
 }
